82277937ab3876e3f112939a912ed3e324300fb4
Jan Phillip Grau <Jan.Grau@sql-service.de>
2020-12-01 08:31:53 +0100

    Day 1b

--- src/day01.rs
+++ src/day01.rs
@@ -10,17 +10,28 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         .collect::<Result<Vec<_>, _>>()?;
 
     let mut result = None;
-
-    'outer: for (idx_a, a) in entries.iter().copied().enumerate() {
+    'outer1: for (idx_a, a) in entries.iter().copied().enumerate() {
         for (idx_b, b) in entries.iter().copied().enumerate() {
             if idx_a != idx_b && a + b == 2020 {
                 result = Some(a * b);
-                break 'outer;
+                break 'outer1;
             }
         }
     }
-
     println!("Day1 a: {:?}", result);
+
+    let mut result = None;
+    'outer2: for (idx_a, a) in entries.iter().copied().enumerate() {
+        for (idx_b, b) in entries.iter().copied().enumerate() {
+            for (idx_c, c) in entries.iter().copied().enumerate() {
+                if idx_a != idx_b && idx_a != idx_c && idx_b != idx_c && a + b + c == 2020 {
+                    result = Some(a * b * c);
+                    break 'outer2;
+                }
+            }
+        }
+    }
+    println!("Day1 b: {:?}", result);
 
     Ok(())
 }
