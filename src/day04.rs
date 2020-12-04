b1daef7bd0efe470699b0de9b192fbb5d2b5d794
Jan Phillip Grau <Jan.Grau@sql-service.de>
2020-12-04 08:35:42 +0100

    Day 4b

--- src/day04.rs
+++ src/day04.rs
@@ -5,13 +5,17 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     let reader = BufReader::new(file);
 
     let mut passport = Passport::default();
-    let mut valid_count = 0;
+    let mut valid_a_count = 0;
+    let mut valid_b_count = 0;
 
     for line in reader.lines() {
         let line = line?;
         if line.is_empty() {
-            if passport.is_valid() {
-                valid_count += 1;
+            if passport.is_valid_a() {
+                valid_a_count += 1;
+            }
+            if passport.is_valid_b() {
+                valid_b_count += 1;
             }
             passport = Passport::default();
         }
@@ -40,11 +44,16 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    if passport.is_valid() {
-        valid_count += 1;
+    if passport.is_valid_a() {
+        valid_a_count += 1;
     }
 
-    println!("Day 4a: {}", valid_count);
+    if passport.is_valid_b() {
+        valid_b_count += 1;
+    }
+
+    println!("Day 4a: {}", valid_a_count);
+    println!("Day 4b: {}", valid_b_count);
 
     Ok(())
 }
@@ -62,7 +71,7 @@ struct Passport {
 }
 
 impl Passport {
-    fn is_valid(&self) -> bool {
+    fn is_valid_a(&self) -> bool {
         self.byr.is_some()
             && self.iyr.is_some()
             && self.eyr.is_some()
@@ -70,5 +79,92 @@ impl Passport {
             && self.hcl.is_some()
             && self.ecl.is_some()
             && self.pid.is_some()
+    }
+
+    fn is_valid_b(&self) -> bool {
+        validate_number(self.byr.as_ref(), 4, 1920, 2002)
+            && validate_number(self.iyr.as_ref(), 4, 2010, 2020)
+            && validate_number(self.eyr.as_ref(), 4, 2020, 2030)
+            && validate_height(self.hgt.as_ref())
+            && validate_hex_color(self.hcl.as_ref())
+            && validate_eye_color(self.ecl.as_ref())
+            && validate_number(self.pid.as_ref(), 9, 0, i32::MAX)
+    }
+}
+
+fn validate_number(item: Option<&String>, len: usize, min: i32, max: i32) -> bool {
+    let item = if let Some(item) = item {
+        item
+    } else {
+        return false;
+    };
+
+    if item.len() != len {
+        return false;
+    }
+
+    let num = match item.parse::<i32>() {
+        Ok(num) => num,
+        Err(_) => return false,
+    };
+
+    min <= num && num <= max
+}
+
+fn validate_height(item: Option<&String>) -> bool {
+    let item = if let Some(item) = item {
+        item
+    } else {
+        return false;
+    };
+
+    if item.len() < 3 {
+        return false;
+    }
+
+    let (num, unit) = item.split_at(item.len() - 2); // NOTE(jpg): asuming ascii input only
+
+    let num = match num.parse::<i32>() {
+        Ok(num) => num,
+        Err(_) => return false,
+    };
+
+    match unit {
+        "cm" => 150 <= num && num <= 193,
+        "in" => 59 <= num && num <= 76,
+        _ => false,
+    }
+}
+
+fn validate_hex_color(item: Option<&String>) -> bool {
+    let item = if let Some(item) = item {
+        item
+    } else {
+        return false;
+    };
+
+    if item.len() != 7 || item.chars().next() != Some('#') {
+        return false;
+    }
+
+    item.chars().skip(1).all(|c| c.is_ascii_hexdigit())
+}
+
+fn validate_eye_color(item: Option<&String>) -> bool {
+    let item = if let Some(item) = item {
+        item
+    } else {
+        return false;
+    };
+
+    match item.as_str() {
+        "amb" => true,
+        "blu" => true,
+        "brn" => true,
+        "gry" => true,
+        "grn" => true,
+        "hzl" => true,
+        "oth" => true,
+        _ => false,
     }
 }
