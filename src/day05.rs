383e96b49a5d9cd0ae8ba998256a6ed1b30a53a6
JP314 <jan.grau95@gmail.com>
2020-12-05 11:16:57 +0100

    Day 5b

--- src/day05.rs
+++ src/day05.rs
@@ -1,15 +1,17 @@
-use std::{error::Error, fs::File, io::BufRead, io::BufReader};
+use std::{
+    collections::HashMap, collections::HashSet, error::Error, fs::File, io::BufRead, io::BufReader,
+};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let file = File::open("./input/input05.txt")?;
     let reader = BufReader::new(file);
 
-    let entries = reader
+    let seats = reader
         .lines()
         .map(|line| -> Result<_, Box<dyn Error>> { Ok(determine_seat_position(line?.as_str())) })
         .collect::<Result<Vec<_>, _>>()?;
 
-    let seat_id = entries
+    let seat_id = seats
         .iter()
         .map(|seat| seat.id())
         .max()
@@ -17,10 +19,33 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 
     println!("Day 5a: {}", seat_id);
 
+    let mut seat_neighbours: HashMap<_, u32> = HashMap::new();
+    for Seat { row, col } in seats.iter().cloned() {
+        let neighbours = [
+            Seat { row: row - 1, col },
+            Seat { row: row + 1, col },
+            Seat { row, col: col - 1 },
+            Seat { row, col: col + 1 },
+        ];
+
+        for n in &neighbours {
+            *seat_neighbours.entry(n.clone()).or_default() += 1;
+        }
+    }
+
+    let all_seats = seats.into_iter().collect::<HashSet<_>>();
+    let my_seat = seat_neighbours
+        .into_iter()
+        .filter(|(seat, count)| *count == 4 && !all_seats.contains(seat))
+        .map(|(seat, _)| seat.id())
+        .collect::<Vec<_>>();
+
+    println!("Day 5b: {:?}", my_seat);
+
     Ok(())
 }
 
-fn binary_partition(mut min: u32, mut max: u32, low: char, high: char, command: &str) -> u32 {
+fn binary_partition(mut min: i32, mut max: i32, low: char, high: char, command: &str) -> i32 {
     for c in command.chars() {
         let mid = (min + max) / 2;
         if c == low {
@@ -40,13 +65,14 @@ fn determine_seat_position(command: &str) -> Seat {
     Seat { row, col }
 }
 
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
 struct Seat {
-    row: u32,
-    col: u32,
+    row: i32,
+    col: i32,
 }
 
 impl Seat {
-    fn id(&self) -> u32 {
+    fn id(&self) -> i32 {
         self.row * 8 + self.col
     }
 }
