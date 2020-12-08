c751331552229ea168761049ebedfa41b7acd047
Jan Phillip Grau <Jan.Grau@sql-service.de>
2020-12-08 08:19:38 +0100

    Day 8b

--- src/day08.rs
+++ src/day08.rs
@@ -3,21 +3,59 @@ use std::{collections::HashSet, error::Error};
 use crate::try_parse_lines;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let instructions = try_parse_lines(8, |line| parse_instruction(&line))?;
-    let mut cpu = Cpu::new(instructions);
+    let mut instructions = try_parse_lines(8, |line| parse_instruction(&line))?;
 
-    let mut visited = HashSet::new();
+    let mut cpu = Cpu::new(&instructions);
+    let mut visited_forward = HashSet::new();
     loop {
-        if !visited.insert(cpu.ip) {
+        if !visited_forward.insert(cpu.ip) {
             break;
         }
-
         cpu.next();
     }
-
     println!("Day 8a: {}", cpu.accumulator);
 
+    let patch = get_patch_address(&instructions, &visited_forward).expect("patch address");
+    let patch_insn = instructions.get_mut(patch).unwrap();
+    match patch_insn {
+        Instruction::Acc(_) => unreachable!(),
+        Instruction::Jmp(x) => *patch_insn = Instruction::Nop(*x),
+        Instruction::Nop(x) => *patch_insn = Instruction::Jmp(*x),
+    }
+
+    let mut cpu = Cpu::new(&instructions);
+    while cpu.ip != cpu.instructions.len() {
+        cpu.next();
+    }
+    println!("Day 8b: {}", cpu.accumulator);
+
     Ok(())
+}
+
+fn get_patch_address(instructions: &[Instruction], candidates: &HashSet<usize>) -> Option<usize> {
+    let mut todo = vec![{
+        let mut cpu = Cpu::new(instructions);
+        cpu.ip = cpu.instructions.len();
+        cpu
+    }];
+
+    while let Some(current) = todo.pop() {
+        for before in current.back() {
+            match before {
+                Ok(before) => {
+                    debug_assert!(!candidates.contains(&before.ip));
+                    todo.push(before);
+                }
+                Err(before) => {
+                    if candidates.contains(&before.ip) {
+                        return Some(before.ip);
+                    }
+                }
+            }
+        }
+    }
+
+    None
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -43,14 +81,15 @@ fn parse_instruction(s: &str) -> Result<Instruction, Box<dyn Error>> {
     Ok(opcode(value))
 }
 
-struct Cpu {
-    instructions: Vec<Instruction>,
+#[derive(Clone)]
+struct Cpu<'a> {
+    instructions: &'a [Instruction],
     ip: usize,
     accumulator: i32,
 }
 
-impl Cpu {
-    fn new(instructions: Vec<Instruction>) -> Self {
+impl<'a> Cpu<'a> {
+    fn new(instructions: &'a [Instruction]) -> Self {
         Self {
             instructions,
             ip: 0,
@@ -71,5 +110,29 @@ impl Cpu {
                 self.ip += 1;
             }
         }
+    }
+
+    fn back(self) -> Vec<Result<Self, Self>> {
+        let mut result = Vec::new();
+
+        for (idx, insn) in self.instructions.iter().copied().enumerate() {
+            let ip_offsets = match insn {
+                Instruction::Acc(_) => vec![1],
+                Instruction::Jmp(x) => vec![x, 1],
+                Instruction::Nop(x) => vec![1, x],
+            };
+
+            for (offset_idx, offset) in ip_offsets.into_iter().enumerate() {
+                if (idx as i32 + offset) as usize != self.ip {
+                    continue;
+                }
+
+                let mut cpu = self.clone();
+                cpu.ip = idx;
+                result.push(if offset_idx == 0 { Ok(cpu) } else { Err(cpu) });
+            }
+        }
+
+        result
     }
 }
