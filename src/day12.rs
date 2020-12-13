075033e42a10e2861c503b4d46068781a84e4921
JP314 <jan.grau95@gmail.com>
2020-12-13 17:17:41 +0100

    Day 12b

--- src/day12.rs
+++ src/day12.rs
@@ -5,7 +5,7 @@ use crate::parse_lines;
 pub fn run() -> Result<(), Box<dyn Error>> {
     let actions = parse_lines(12, |s| parse_action(&s))?;
 
-    let mut ship = Ship {
+    let mut ship = ShipA {
         direction: Direction::East,
         x: 0,
         y: 0,
@@ -15,8 +15,20 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         ship.eval(action);
     }
 
-    let result = ship.x + ship.y;
-    println!("Day 12a: {}", result);
+    println!("Day 12a: {}", ship.x.abs() + ship.y.abs());
+
+    let mut ship = ShipB {
+        waypoint_x: 10,
+        waypoint_y: -1,
+        ship_x: 0,
+        ship_y: 0,
+    };
+
+    for action in actions.iter().copied() {
+        ship.eval(action);
+    }
+
+    println!("Day 12b: {}", ship.ship_x.abs() + ship.ship_y.abs());
 
     Ok(())
 }
@@ -38,13 +50,13 @@ enum Direction {
     South,
 }
 
-struct Ship {
+struct ShipA {
     direction: Direction,
     x: i32,
     y: i32,
 }
 
-impl Ship {
+impl ShipA {
     fn eval(&mut self, action: Action) {
         match action {
             Action::Move(direction, distance) => self.move_in_direction(direction, distance),
@@ -71,6 +83,48 @@ impl Ship {
 
         let direction = (self.direction as i32 + (angle / 90)).rem_euclid(4);
         self.direction = unsafe { std::mem::transmute(direction as u8) };
+    }
+}
+
+struct ShipB {
+    waypoint_x: i32,
+    waypoint_y: i32,
+    ship_x: i32,
+    ship_y: i32,
+}
+
+impl ShipB {
+    fn eval(&mut self, action: Action) {
+        match action {
+            Action::Move(Direction::North, value) => {
+                self.waypoint_y -= value;
+            }
+            Action::Move(Direction::South, value) => {
+                self.waypoint_y += value;
+            }
+            Action::Move(Direction::West, value) => {
+                self.waypoint_x -= value;
+            }
+            Action::Move(Direction::East, value) => {
+                self.waypoint_x += value;
+            }
+            Action::Left(angle) => self.turn(angle),
+            Action::Right(angle) => self.turn(-angle),
+            Action::Forward(value) => {
+                self.ship_x += self.waypoint_x * value;
+                self.ship_y += self.waypoint_y * value;
+            }
+        }
+    }
+
+    fn turn(&mut self, angle: i32) {
+        debug_assert_eq!(angle % 90, 0);
+        let direction = (angle / 90).rem_euclid(4);
+        for _ in 0..direction {
+            let (x, y) = (self.waypoint_x, self.waypoint_y);
+            self.waypoint_x = y;
+            self.waypoint_y = -x;
+        }
     }
 }
 
