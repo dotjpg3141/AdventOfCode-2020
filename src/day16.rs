b3cf3c73fc5a6f059915ebc0ffa822de569cb7fa
JP314 <jan.grau95@gmail.com>
2020-12-16 11:10:14 +0100

    Day 16b

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,8 @@
-use std::{error::Error, ops::RangeInclusive};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    ops::RangeInclusive,
+};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let input = std::fs::read_to_string("./input/input16.txt")?;
@@ -19,6 +23,9 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     let result = solve1(&rules, &nearby_tickets);
     println!("Day 16a: {}", result);
 
+    let result = solve2(&rules, nearby_tickets, my_ticket);
+    println!("Day 16b: {}", result);
+
     Ok(())
 }
 
@@ -29,6 +36,61 @@ fn solve1(rules: &[Rule], nearby: &[Ticket]) -> u64 {
         .copied()
         .filter(|number| rules.iter().all(|rule| !rule.contains(*number)))
         .sum()
+}
+
+fn solve2(rules: &[Rule], nearby: Vec<Ticket>, my_ticket: Ticket) -> u64 {
+    let valid_tickets = nearby.iter().filter(|ticket| ticket.is_valid(rules));
+
+    let mut votes = vec![HashMap::<_, usize>::new(); rules.len()];
+    for ticket in valid_tickets.clone() {
+        for (idx, number) in ticket.numbers.iter().copied().enumerate() {
+            for rule in rules.iter().filter(|rule| rule.contains(number)) {
+                *votes[idx].entry(rule.name.as_str()).or_default() += 1;
+            }
+        }
+    }
+
+    let mut result = 1;
+    let mut visited_rules = HashSet::new();
+    let valid_ticket_count = valid_tickets.count();
+
+    while let Some((idx, rule_name)) = find_unique_vote(&votes, valid_ticket_count, &visited_rules)
+    {
+        if rule_name.starts_with("departure") {
+            result *= my_ticket.numbers[idx];
+        }
+
+        visited_rules.insert(rule_name);
+    }
+
+    debug_assert_eq!(visited_rules.len(), rules.len());
+
+    result
+}
+
+fn find_unique_vote<'a>(
+    votes: &[HashMap<&'a str, usize>],
+    ticket_count: usize,
+    visited_rules: &HashSet<&str>,
+) -> Option<(usize, &'a str)> {
+    votes.iter().enumerate().find_map(|(idx, votes)| {
+        get_unique(votes.iter().filter_map(|(rule_name, count)| {
+            if *count == ticket_count && !visited_rules.contains(rule_name) {
+                Some((idx, *rule_name))
+            } else {
+                None
+            }
+        }))
+    })
+}
+
+fn get_unique<T>(mut iter: impl Iterator<Item = T>) -> Option<T> {
+    let maybe_unique = iter.next()?;
+    if iter.next().is_none() {
+        Some(maybe_unique)
+    } else {
+        None
+    }
 }
 
 struct Rule {
@@ -45,6 +107,14 @@ impl Rule {
 
 struct Ticket {
     numbers: Vec<u64>,
+}
+
+impl Ticket {
+    fn is_valid(&self, rules: &[Rule]) -> bool {
+        self.numbers
+            .iter()
+            .all(|number| rules.iter().any(|rule| rule.contains(*number)))
+    }
 }
 
 fn parse_rule(s: &str) -> Rule {
