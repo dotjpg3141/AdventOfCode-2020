ca705508f25bb5ab7a47a11f303f599c71277053
Jan Phillip Grau <Jan.Grau@sql-service.de>
2020-12-18 13:14:25 +0100

    Day 18b

--- src/day18.rs
+++ src/day18.rs
@@ -1,12 +1,20 @@
 use std::{error::Error, iter::Peekable};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let input = std::fs::read_to_string("./input/input18.txt")?
-        .lines()
-        .map(parse_expression_from_str)
-        .collect::<Vec<_>>();
+    let input = std::fs::read_to_string("./input/input18.txt")?;
 
-    let result = input.iter().map(Expression::eval).sum::<i64>();
+    let result = input
+        .lines()
+        .map(|s| parse_expression_from_str(s, &|_| 1))
+        .map(|expr| expr.eval())
+        .sum::<i64>();
+    println!("Day 18a: {}", result);
+
+    let result = input
+        .lines()
+        .map(|s| parse_expression_from_str(s, &|binary| if binary == "+" { 2 } else { 1 }))
+        .map(|expr| expr.eval())
+        .sum::<i64>();
     println!("Day 18a: {}", result);
 
     Ok(())
@@ -28,18 +36,25 @@ impl Expression {
     }
 }
 
-fn parse_expression_from_str(s: &str) -> Expression {
+fn parse_expression_from_str(s: &str, precedence_table: &impl Fn(&str) -> usize) -> Expression {
     let s = s.replace("(", " ( ").replace(")", " ) ");
     let mut tokens = s.split(' ').filter(|s| !s.is_empty()).peekable();
-    parse_expression(&mut tokens)
+    parse_expression(&mut tokens, precedence_table)
 }
 
-fn parse_expression<'a>(tokens: &mut Peekable<impl Iterator<Item = &'a str>>) -> Expression {
-    parse_binary(tokens)
+fn parse_expression<'a>(
+    tokens: &mut Peekable<impl Iterator<Item = &'a str>>,
+    precedence_table: &impl Fn(&str) -> usize,
+) -> Expression {
+    parse_binary(tokens, precedence_table, 0)
 }
 
-fn parse_binary<'a>(tokens: &mut Peekable<impl Iterator<Item = &'a str>>) -> Expression {
-    let mut lhs = parse_primary(tokens);
+fn parse_binary<'a>(
+    tokens: &mut Peekable<impl Iterator<Item = &'a str>>,
+    precedence_table: &impl Fn(&str) -> usize,
+    min_precedence: usize,
+) -> Expression {
+    let mut lhs = parse_primary(tokens, precedence_table);
     while let Some(&token) = tokens.peek() {
         let binary = match token {
             "+" => Expression::Adition,
@@ -47,19 +62,27 @@ fn parse_binary<'a>(tokens: &mut Peekable<impl Iterator<Item = &'a str>>) -> Exp
             _ => break,
         };
 
+        let precedence = precedence_table(token);
+        if precedence <= min_precedence {
+            break;
+        }
+
         tokens.next().unwrap();
 
-        let rhs = parse_primary(tokens);
+        let rhs = parse_binary(tokens, precedence_table, precedence);
         lhs = binary(Box::new(lhs), Box::new(rhs));
     }
     lhs
 }
 
-fn parse_primary<'a>(tokens: &mut Peekable<impl Iterator<Item = &'a str>>) -> Expression {
+fn parse_primary<'a>(
+    tokens: &mut Peekable<impl Iterator<Item = &'a str>>,
+    precedence_table: &impl Fn(&str) -> usize,
+) -> Expression {
     match *tokens.peek().expect("unexpected end of file") {
         "(" => {
             tokens.next().unwrap();
-            let expression = parse_expression(tokens);
+            let expression = parse_expression(tokens, precedence_table);
             assert!(tokens.next() == Some(")"));
             expression
         }
